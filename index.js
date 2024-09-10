16e4089cc9add1523422bdcc2a81ceba8b3d73a8
Klafke96 <arthurbklafke@gmail.com>
2024-09-10 12:10:23 -0300

    template aula 1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-let greetings = "Hello sir!"
-greetings = "Hello miss"
-console.log(greetings);
+let meta = {
+    value: "Correr pelo menos uma vez na semana",
+    checked: false
+}
+
+let metas = [
+    meta,
+    {
+    value: "Ler pelo menos uma vez no dia",
+        checked: false
+    }
+]
+
+console.log(metas[0].value)
