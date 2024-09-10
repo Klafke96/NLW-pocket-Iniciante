519133a5d4fe99755562e78be9d4141dbe74b00a
Klafke96 <arthurbklafke@gmail.com>
2024-09-10 14:06:20 -0300

    adiciona inquirer

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 function start(){
-    let count = 0
-    while(count < 10){
-        console.log(count)
-        count++
+
+    console.log("-----------------MENU----------------")
+    console.log("1 - Cadastrar")
+    console.log("2 - Listar")
+    console.log("3 - Sair")
+    console.log("------------------------------------")
+    console.log(" ")
+
+    while(true){
+        let opcao = "cadastrar"
+        switch(opcao){
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                break
+            case "sair":
+                return
+        }
     }
 }
 
